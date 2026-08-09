61ce55ee3769a6e8861deceac6daf0e9e70cb15f
Aziz <azizplays895586@gmail.com>
2026-08-09 17:13:46 +0530

    updated changes

---
+++ spread.js
@@ -0,0 +1,7 @@
+let arr = [1, 4, 14];
+let arr1 = [...arr];
+let arr3 = [1, 123, 1, 3];
+let arr4 = [7, 7, ...arr3];
+export {};
+console.log(arr1)
+console.log(arr4)
---
+++ spread.ts
@@ -0,0 +1,6 @@
+let arr=[1,4,14]
+let arr1=[...arr]//here this array will print the values of arrr bcz it copies it also work for combining
+
+let arr3=[1,123,1,3]
+let arr4=[7,7, ...arr3]// now it willl give the value of both of arr3 and arr4
+export{}
